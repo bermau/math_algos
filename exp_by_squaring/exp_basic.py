b4a79360b92d2722b7651331bb6a2589689f1581
Sharat M R <cosmosgenius@gmail.com>
2013-07-13 20:14:33 +0530

    Completed finding Exponent by squaring

--- exp_by_squaring/exp_basic.py
+++ exp_by_squaring/exp_basic.py
@@ -3,10 +3,24 @@ import argparse
 
 def create_cmdarg():
     parser = argparse.ArgumentParser(description='Find Exponent')
-    parser.add_argument('base', help='Base Value')
-    parser.add_argument('exponent', help='Exponent Value')
+    parser.add_argument('base', type=float, help='Base Value')
+    parser.add_argument('exponent', type=int, help='Exponent Value')
     args = parser.parse_args()
-    return args
+    return (args.base, args.exponent)
+
+
+def exp(base, exponent):
+    if exponent < 0:
+        return exp(1/base, -exponent)
+    elif exponent == 0:
+        return 1
+    elif exponent == 1:
+        return base
+    elif exponent % 2 == 0:
+        return exp(base*base, exponent/2)
+    elif exponent % 2 == 1:
+        return base * exp(base*base, (exponent-1)/2)
 
 if __name__ == '__main__':
-    create_cmdarg()
+    base, exponent = create_cmdarg()
+    print (exp(base, exponent))
