6990b1cd27460edefcbcc884bd96fdf3e69f9baa
Praseetha-KR <voidimagineer@yahoo.com>
2014-07-25 00:33:04 +0530

    Modinv implemented with extended euclidean algorithm

--- rsa.py
+++ rsa.py
@@ -9,6 +9,25 @@ def is_prime(x):
 		i += 1
 	return True
 
+def gcd(a, b):
+	while b:
+		a, b = b, a%b
+	return a
+
+def egcd(a, b):
+	x, y, u, v = 0, 1, 1, 0
+	while a != 0:
+		q, r = b//a, b%a
+		m, n = x-u*q, y-v*q
+		b,a,x,y,u,v = a,r,u,v,m,n
+	return b,x,y
+
+def modinv(a,m):
+	g,x,y = egcd(a,m)
+	if g != 1:
+		return None
+	else:
+		return x%m
 
 if __name__ == "__main__":
 	p = input("Enter p: ")
@@ -19,15 +38,17 @@ if __name__ == "__main__":
 	while True:
 		r = randint(2,n1)
 		if is_prime(r):
-			break
+			if gcd(r, n1) == 1:
+				break
+
 	e = r
 	print("e = %d" % e)
-
-	d = (1 + n1)/e
+	# d = (1 + n1)/e
+	d = modinv(e, n1)
 	print("d = %d" % d)
 
 	m = input("Enter message: ")
 	c = (pow(m, e)) % n
 	print("Encrypted message = %d" % c)
-	m1 = (pow(c, p)) % n
+	m1 = (pow(c, d)) % n
 	print("Decrypted message = %d" % m1)
