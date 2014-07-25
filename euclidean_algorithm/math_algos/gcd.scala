8e6de47e47f068f3448270ae00b13188531c7367
Sharat M R <cosmosgenius@gmail.com>
2014-07-26 01:23:04 +0530

    added recursive gcd using Euclidean Algorithm

--- euclidean_algorithm/math_algos/gcd.scala
+++ euclidean_algorithm/math_algos/gcd.scala
@@ -2,6 +2,20 @@ package math_algos
 
 object gcd {
     def main(args: Array[String]) {
-    	println("hello");
+    	if(args.length < 2){
+    	    println("Requires two integers as parameters");
+    		return
+    	}
+    	val num1 = args(0).toInt;
+    	val num2 = args(1).toInt;
+    	
+    	println(gcd(num1,num2));
+    }
+    
+    def gcd(num1: Int, num2: Int) : Int = {
+        if(num2 == 0){
+            return num1;
+        }
+        return gcd(num2, num1 % num2);
     }
 }
